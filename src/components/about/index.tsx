0a94633e96e6fee0ce23e8d0aabff3434eb4e714
Adriano Miranda <adrjmiranda@gmail.com>
2024-04-02 11:35:40 -0300

    chore: added about area

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -2,9 +2,68 @@ import { FunctionComponent, ReactNode } from 'react';
 
 // Style
 import * as Style from './style';
+import { SessionTitle, Wrapper } from '../../style/Components';
+
+// Images
+import OurAgency from '../../assets/img/our-agency.png';
+import WaveTop from '../../assets/img/wave-top.png';
+import WaveBottom from '../../assets/img/wave-bottom.png';
+
+// Icons
+import { SiAntdesign } from 'react-icons/si';
 
 const About: FunctionComponent = (): ReactNode => {
-	return <Style.Container>About</Style.Container>;
+	return (
+		<Style.Container>
+			<Style.WaveTop>
+				<img src={WaveTop} alt='Wave Top' />
+			</Style.WaveTop>
+			<SessionTitle>Why choose us?</SessionTitle>
+			<Wrapper>
+				<Style.Content>
+					<Style.List>
+						<h4>Our difference</h4>
+						<ul>
+							<li>
+								<SiAntdesign />
+								<span>Customized strategy</span>
+							</li>
+							<li>
+								<SiAntdesign />
+								<span>Innovative creativity</span>
+							</li>
+							<li>
+								<SiAntdesign />
+								<span>Attractive design</span>
+							</li>
+							<li>
+								<SiAntdesign />
+								<span>Cutting-edge technology</span>
+							</li>
+							<li>
+								<SiAntdesign />
+								<span>Captivating content</span>
+							</li>
+							<li>
+								<SiAntdesign />
+								<span>Impactful social presence</span>
+							</li>
+							<li>
+								<SiAntdesign />
+								<span>Measurable results</span>
+							</li>
+						</ul>
+					</Style.List>
+					<Style.Img>
+						<img src={OurAgency} alt='Our Agency' />
+					</Style.Img>
+				</Style.Content>
+			</Wrapper>
+			<Style.WaveBottom>
+				<img src={WaveBottom} alt='Wave Bottom' />
+			</Style.WaveBottom>
+		</Style.Container>
+	);
 };
 
 export default About;
--- src/components/about/style.ts
+++ src/components/about/style.ts
@@ -1,3 +1,84 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+	padding: 380px 0;
+	background-color: rgba(0, 0, 0, 0.05);
+	position: relative;
+`;
+
+export const Content = styled.div`
+	margin-top: 4rem;
+	display: flex;
+	gap: 4rem;
+`;
+
+export const List = styled.div`
+	flex: 50%;
+	display: flex;
+	flex-direction: column;
+	gap: 2rem;
+	z-index: 1;
+
+	h4 {
+		font-size: 2.5rem;
+		text-transform: capitalize;
+		font-family: ${(props) => props.theme.fontFamily.lilitaOne};
+		color: ${(props) => props.theme.color.hotPink};
+	}
+
+	ul {
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+	}
+
+	ul li {
+		font-size: ${(props) => props.theme.fontSize.fsLarge};
+		font-weight: ${(props) => props.theme.fontWeight.fwMedium};
+		text-transform: capitalize;
+		background-color: ${(props) => props.theme.color.skyBlue};
+		color: ${(props) => props.theme.color.neutralWhite};
+		padding: 0.4rem 0.8rem;
+		border-radius: ${(props) => props.theme.borderRadius.brSmall};
+		display: flex;
+		align-items: center;
+		gap: 1rem;
+		transition: ${(props) => props.theme.transitionType.trMedium};
+
+		&:hover {
+			transform: scale(1.1);
+		}
+	}
+
+	ul li svg {
+		color: ${(props) => props.theme.color.aquaBlue};
+		transition: ${(props) => props.theme.transitionType.trMedium};
+	}
+
+	ul li:hover svg {
+		transform: rotate(45deg);
+		color: white;
+	}
+`;
+
+export const Img = styled.div`
+	flex: 50%;
+
+	img {
+		max-width: 100%;
+	}
+`;
+
+export const WaveTop = styled.div`
+	img {
+		position: absolute;
+		top: 0;
+	}
+`;
+
+export const WaveBottom = styled.div`
+	img {
+		position: absolute;
+		bottom: 0;
+	}
+`;
